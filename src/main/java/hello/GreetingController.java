55fdf824a06527c506c566fe869380bca4120f31
Georgiy-Vwfe <postnikov.george@yandex.ru>
2019-04-03 10:25:31 +0300

    more news

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,19 +17,13 @@ public class GreetingController {
 
     private NewsRepository repository;
 
-    @PostConstruct
-    public void init() {
-        repository = new FileNewsRepository(
-                ClassLoader.getSystemResourceAsStream("news.txt"));
-    }
-
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("section", "greeting");
         return "greeting";
@@ -36,12 +31,22 @@ public class GreetingController {
 
     @GetMapping("/news")
     public String news(Model model) throws IOException {
-        List<News> news = repository.readNews();
+        fileToModel("news.txt", model);
+        return "news";
+    }
 
+    @GetMapping("/news/{file}")
+    public String news1(Model model, @PathVariable("file") String fileName) throws IOException {
+        fileToModel(fileName, model);
+        return "news";
+    }
+
+    private void fileToModel(String fileName, Model model) throws IOException {
+        repository = new FileNewsRepository(
+                ClassLoader.getSystemResourceAsStream(fileName));
+        List<News> news = repository.readNews();
         model.addAttribute("news", news);
         model.addAttribute("section", "news");
-
-        return "news";
     }
 
     @PostMapping
